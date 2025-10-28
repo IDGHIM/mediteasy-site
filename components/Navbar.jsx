d506506926805d986addd148d3246520e4781b3e
OlivierF47 <olivier.forabosco05@gmail.com>
2025-10-28 09:23:48 +0100

    "mise en place navbar"

---
+++ components/Navbar.jsx
@@ -0,0 +1,18 @@
+import styles from "./styles/Navbar.module.css"
+
+
+export default Navbar = () => {
+    return(
+        <>
+            <nav>
+                <ul>
+                    <li>Projet</li>
+                    <li>Téléchargement</li>
+                    <li>Dev</li>
+                    <li>Contribution</li>
+                    <li>Nous contacter</li>
+                </ul>
+            </nav>
+        </>
+    )
+}
